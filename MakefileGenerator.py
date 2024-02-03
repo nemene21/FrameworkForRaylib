296b09759156d134ca94a047119f0437c1e13498
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-02-03 12:28:33 +0100

    UI for semi-auto makefile generatin, compiling and running

--- MakefileGenerator.py
+++ MakefileGenerator.py
@@ -1,3 +1,5 @@
+import tkinter, subprocess
+
 CFLAGS   = "-O1 -Wall -Wno-missing-braces -I../include/"
 LDFLAGS  = "-L../lib/"
 LDLIBS   = "-lraylib -lopengl32 -lgdi32 -lwinmm"
@@ -6,13 +8,6 @@ SRC_DIR  = "../src"
 COMPILER = "g++"
 
 makefile = ""
-
-source_files = [
-    "main",
-    "entity",
-    "component",
-    "test/test"
-]
 
 def next(n=1):
     global makefile
@@ -27,27 +22,79 @@ def add(cont):
     global makefile
     makefile += cont
 
-add("all: Build.exe")
-next(2)
+def generate_makefile():
+    global makefile
 
-for source in source_files:
-    add(f"{OBJ_DIR}/{source}.o: {SRC_DIR}/{source}.cpp")
-    next(); tab()
+    print("Starting...")
+    source_list_file = open("SourceList", "r")
+    source_files = source_list_file.read().split("\n")
 
-    add(f"{COMPILER} {CFLAGS} -c {SRC_DIR}/{source}.cpp -o $(@)")
+    source_list_file.close()
+    print("Opened Source List...")
+
+    makefile = ""
+
+    add("all: Build.exe")
     next(2)
 
-add("Build.exe: ")
-for source in source_files:
-    add(OBJ_DIR + "/" + source + ".o ")
+    for source in source_files:
+        add(f"{OBJ_DIR}/{source}.o: {SRC_DIR}/{source}.cpp")
+        next(); tab()
 
-next(); tab()
-add(f"{COMPILER} -o Build.exe ")
-for source in source_files:
-    add(OBJ_DIR + "/" + source + ".o ")
+        add(f"{COMPILER} {CFLAGS} -c {SRC_DIR}/{source}.cpp -o $(@)")
+        next(2)
 
-add(LDFLAGS + " " + LDLIBS)
+    add("Build.exe: ")
+    for source in source_files:
+        add(OBJ_DIR + "/" + source + ".o ")
 
-file = open("build/Makefile", "w")
-file.write(makefile)
-file.close()
+    next(); tab()
+    add(f"{COMPILER} -o Build.exe ")
+    for source in source_files:
+        add(OBJ_DIR + "/" + source + ".o ")
+
+    add(LDFLAGS + " " + LDLIBS)
+
+    print("Generated...")
+
+    file = open("build/Makefile", "w")
+    file.write(makefile)
+    file.close()
+
+    print("Saved...\n")
+
+def make():
+    result = subprocess.run("make", shell=True, cwd="build")
+
+    if result:
+        print("Make failed :(")
+    else:
+        print("Project sucessfully build! :)")
+    print("")
+
+def run():
+    result = subprocess.run("Build.exe", shell=True, cwd="build")
+
+    if result:
+        print("Game Sucessfully Ran!")
+    else:
+        print("Game didnt' Run :(")
+    print("")
+
+window = tkinter.Tk()
+window.configure(bg='#333333')
+window.geometry("512x512")
+window.title("Auto Compiling and Running!")
+
+font = ("Helvetica", 24)
+
+generate_button = tkinter.Button(window, text="Generate Makefile", command=generate_makefile, font=font)
+generate_button.place(x = 16, y = 16)
+
+make_button = tkinter.Button(window, text="Make", command=make, font=font)
+make_button.place(x = 16, y = 16 + 80)
+
+run_button = tkinter.Button(window, text="Run", command=run, font=font)
+run_button.place(x = 16, y = 16 + 160)
+
+window.mainloop()
