1383e7e238b341c4593f8ee5b04aedca2ddca283
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-02-04 13:25:07 +0100

    fps counter, over 1000 :D

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,23 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <trails.h>
+#include <string>
 
 int main() {
     const int window_width = 1920*.5, window_height = 1080*.5;
     InitWindow(window_width, window_height, "Raylib Window!");
 
-    Trail trail = Trail({0, 0}, 12, 128, WHITE);
+    Trail trail = Trail({0, 0}, 12, 128, WHITE, BLACK);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(BLACK);
+
+        std::string fps_string = std::to_string(GetFPS());
+
+        const char* fps_cstring = fps_string.c_str();
+        SetWindowTitle(fps_cstring);
 
         trail.position = GetMousePosition();
         trail.process();
