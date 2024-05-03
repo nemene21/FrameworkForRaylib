031ef2d542a59a97b934de29782624f578939fa6
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-05-03 20:58:46 +0200

    Added material class

---
+++ src/framework/objects/drawables.hpp
@@ -0,0 +1,53 @@
+#ifndef DRAWABLES_H
+#define DRAWABLES_H
+
+#include <raylib.h>
+#include <raymath.h>
+#include <map>
+#include <iostream>
+#include <string>
+#include <memory>
+
+#define SHADER_DIR  (std::string)("assets/shaders/") +
+
+typedef std::shared_ptr<Shader> ShaderPtr;
+
+class ShaderManager {
+public:
+    static std::map<
+        std::string,
+        ShaderPtr
+    > shader_map;
+
+    static float timer;
+    static float tick;
+
+    static void load(std::string path);
+    static void unload(std::string path);
+    static ShaderPtr get(std::string path);
+
+    static void unload_check();
+    static void unload_unused();
+    static void unload_all();
+    static void reload();
+
+    static void update_uniforms();
+};
+
+class Material {
+public:
+    ShaderPtr shader;
+
+    Material(std::string shader_path);
+    Material(ShaderPtr shader);
+
+    virtual void set_shader(std::string shader_path);
+    virtual void set_shader(ShaderPtr new_shader);
+
+    virtual void use();
+    virtual void process(float delta);
+
+    ShaderPtr get_shader();
+};
+
+#endif
--- src/framework/objects/sprites.hpp
+++ src/framework/objects/sprites.hpp
@@ -1,15 +1,9 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include <raylib.h>
-#include <raymath.h>
-#include <map>
-#include <iostream>
-#include <string>
-#include <memory>
+#include <drawables.hpp>
 
 #define TEXTURE_DIR (std::string)("assets/images/") +
-#define SHADER_DIR  (std::string)("assets/shaders/") +
 
 void DrawTextureCentered(Texture2D *texture_ptr, Vector2 position, Vector2 scale={1, 1}, float angle=0, Color tint=WHITE);
 void DrawTextureSheet(
@@ -21,30 +15,6 @@ void DrawTextureSheet(
     float angle=0,
     Color tint=WHITE
 );
-
-typedef std::shared_ptr<Shader> ShaderPtr;
-
-class ShaderManager {
-public:
-    static std::map<
-        std::string,
-        ShaderPtr
-    > shader_map;
-
-    static float timer;
-    static float tick;
-
-    static void load(std::string path);
-    static void unload(std::string path);
-    static ShaderPtr get(std::string path);
-
-    static void unload_check();
-    static void unload_unused();
-    static void unload_all();
-    static void reload();
-
-    static void update_uniforms();
-};
 
 
 typedef std::shared_ptr<Texture2D> TexturePtr;
