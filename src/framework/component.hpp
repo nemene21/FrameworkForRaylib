d13f1d1c1cceeae088245a9c57a03be1bc608e41
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-06-27 14:17:52 +0200

    Docs for component.hpp

--- src/framework/component.hpp
+++ src/framework/component.hpp
@@ -12,18 +12,23 @@ class Entity;
 
 typedef std::function<void(Entity *)> SignalFunction;
 
-// <Signals>
+/// @brief Calls callbacks when a certain event happens
 class Signal {
 protected:
     std::vector<SignalFunction> callers;
 
 public:
     Signal();
+    /// @brief Connect a callback function to the signal
+    /// @param func Pointer to callback
+    /// @note Callbacks return nothing and have only one parameter (Entity*)
     void connect(SignalFunction func);
+    /// @brief Calls all the callbacks (event happened)
+    /// @param parent The entity the callbacks referance
     void emit(Entity* parent);
 };
 
-// <Component Superclass>
+/// @brief All component types
 enum ComponentType {
     TRANSFORM,
     VELOCITY,
@@ -33,11 +38,14 @@ enum ComponentType {
     COLLIDER,
     AREA,
     TIMER,
+    /// @brief Number of components in framework
     COUNT,
 };
 
 typedef ComponentType CompType;
 
+/// @brief Component object
+/// @note This is not a pure ECS, the components hold both data and functionality
 class Component {
 public:
     ComponentType type;
@@ -46,6 +54,7 @@ public:
     Component(ComponentType type, Entity *entity);
     virtual ~Component();
 
+    /// @brief Adds the component to its entity
     virtual void setup();
 
     virtual void process(float delta);
@@ -58,6 +67,7 @@ typedef std::set<Component *> ComponentSet;
 typedef std::map<ComponentType, ComponentSet>
     ComponentMap;
 
+/// @brief Is responsible for component queries
 class ComponentManager {
 protected:
     static ComponentMap component_map;
@@ -66,9 +76,16 @@ public:
     static void add_component(Component *comp);
     static void remove_component(Component *comp);
 
+    /// @brief Returns a set of components of a certain type
+    /// @param type CompType::YOUR_COMPONENT_TYPE
+    /// @return Set of components
     static ComponentSet query_components(ComponentType type);
-
+    /// @brief Returns how many components of "type" type there are
+    /// @param type CompType::YOUR_COMPONENT_TYPE
+    /// @return Number of components
     static int component_type_count(ComponentType type);
+    /// @brief Returns the number of all components
+    /// @return Number of all components
     static int component_count();
 };
 
