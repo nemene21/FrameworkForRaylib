1a8f77c5efdeca282d11241ab50d99505c3344b0
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-06-27 14:23:18 +0200

    Docs for entity.hpp

--- src/framework/entity.hpp
+++ src/framework/entity.hpp
@@ -9,6 +9,7 @@
 
 class Component;
 
+/// @brief Object in a scene, it has a map of components that are automatically processed and drawn
 class Entity {
 protected:
     std::map<ComponentType, Component*> comps;
@@ -22,17 +23,41 @@ public:
 
     virtual void process_components(float delta);
     virtual void draw_components(float delta);
- 
+    
+    /// @brief Makes the entity be in a group
+    /// @param group Group name
+    /// @note Groups are used to query components of a certain group or to check if an entity should have a certain functionality
     virtual void join_group(std::string group);
+    /// @brief Makes the entity leave a group
+    /// @param group Group name
     virtual void leave_group(std::string group);
+    /// @brief Checks if the entity is in the group
+    /// @param group Group name
+    /// @retval TRUE Is in the group
+    /// @retval FALSE Is not in the group
     virtual bool is_in_group(std::string group);
 
+    /// @brief Deletes the entity
     virtual void queue_free();
+    /// @brief Returns true if the entity is queued for deletion
+    /// @retval TRUE Is queued for deletion
+    /// @retval FALSE Is not queued for deletion
     virtual bool is_death_queued();
 
+    /// @brief Adds the component to the entity
+    /// @param comp Component pointer
     virtual void add_component(Component* comp);
+    /// @brief Removes the component of a certain type
+    /// @param type CompType::YOUR_COMPONENT_TYPE
     virtual void remove_component(ComponentType type);
+    /// @brief Returns true if the entity has a certain component
+    /// @param type CompType::YOUR_COMPONENT_TYPE
+    /// @retval TRUE Entity has a certain component
+    /// @retval FALSE Entity doesn't have a certain component
     virtual bool has_component(ComponentType type);
+    /// @brief Returns a pointer to the wanted component
+    /// @param type CompType::YOUR_COMPONENT_TYPE
+    /// @return Pointer to component
     virtual Component *get_component(ComponentType type);
 
     virtual void process(float delta);
