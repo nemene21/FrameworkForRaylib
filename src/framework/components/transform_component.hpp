e050494b90c2950ea202d5e17c8be28e4a515449
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-06-28 20:05:06 +0200

    Doxygen comments for transform_component.hpp

--- src/framework/components/transform_component.hpp
+++ src/framework/components/transform_component.hpp
@@ -16,11 +16,23 @@ public:
 
     TransformComponent(Entity *entity, Vector2 position = {0, 0}, Vector2 scale = {1, 1}, float angle = 0);
 
+    /// @brief Adds vector to position
+    /// @param adding Vector to add
     void translate(Vector2 adding);
+    /// @brief Adds to x position
+    /// @param adding X to add
     void translate_x(float adding);
+    /// @brief Adds to y position
+    /// @param adding Y to add
     void translate_y(float adding);
 
+    /// @brief Smoothly interpolates the velocity of the transform
+    /// @param to The vector to interpolate the velocity to
+    /// @param speed The speed of the interpolation
+    /// @note This is frame independant
     void interpolate_velocity(Vector2 to, float speed);
+    /// @brief Add a speed to the velocity
+    /// @param by Speed to add
     void accelerate(Vector2 by);
 
     void process(float delta);
